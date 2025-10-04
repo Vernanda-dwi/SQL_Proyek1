ba595a1dc0312874b0bfcd0a52041e91eaf3eb95
Vernanda <168617329+Vernanda-dwi@users.noreply.github.com>
2025-10-04 16:11:24 +0700

    Update 2_Select Statement Dasar.sql

--- ITBOX_Course/2_Select Statement Dasar.sql
+++ ITBOX_Course/2_Select Statement Dasar.sql
@@ -15,3 +15,30 @@ where merk not in ('Samsung','Apple')
 
 
 -- Perintah Like
+select * from itbox_course.pelanggan p 
+where alamat like '%No%'
+-- Kolom alamat yang mengandung kata No
+
+select * from itbox_course.pelanggan p 
+where alamat like '%Pondok Indah%'
+
+-- Between
+-- menarik data hanya untuk rentang menimum samai maksimum. Dpaat digunakan pada data numerik,teks dan tanggal
+-- and nya dianggap bukan operator logika
+select* from itbox_course.transaksi_header th 
+where total_harga  between 800000 and 1000000
+
+select* from itbox_course.transaksi_header th 
+where total_barang  between 2 and 3
+	and total_harga > 800000
+	
+
+-- Order By
+-- mengurutkan data baik numerik , tek, tanggal
+
+select* from itbox_course.transaksi_header th 
+order by 
+ tanggal_transaksi desc
+ ,total_barang desc
+ 
+
